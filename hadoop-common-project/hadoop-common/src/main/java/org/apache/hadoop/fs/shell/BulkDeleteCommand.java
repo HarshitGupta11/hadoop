d91fcb463082b045ed0e6e64f14d8c2e90aaf5e8
harshit.gupta <harshit.gupta@cloudera.com>
2024-12-06 20:07:09 +0530

    Basic Nit Review Fixes with Page Size Implementation

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/shell/BulkDeleteCommand.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/shell/BulkDeleteCommand.java
@@ -21,9 +21,7 @@ package org.apache.hadoop.fs.shell;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import org.apache.hadoop.conf.Configuration;
@@ -40,13 +38,18 @@ public class BulkDeleteCommand extends FsCommand {
 
     public static final String READ_FROM_FILE = "readFromFile";
 
-    public static final String USAGE = "-[ " + READ_FROM_FILE + "] [<file>] [<basePath> <paths>]";
+    public static final String PAGE_SIZE = "pageSize";
+
+    public static final String USAGE = "-[ " + READ_FROM_FILE + "] [<file>] [" +
+        PAGE_SIZE + "] [<pageSize>] [<basePath> <paths>]";
 
     public static final String DESCRIPTION = "Deletes the set of files under the given path. If a list of paths " +
             "is provided then the paths are deleted directly. User can also point to the file where the paths are" +
             "listed as full object names.";
 
     private String fileName;
+
+    private int pageSize;
 
     /*
     Making the class stateful as the PathData initialization for all args is not needed
@@ -63,8 +66,14 @@ public class BulkDeleteCommand extends FsCommand {
     protected void processOptions(LinkedList<String> args) throws IOException {
         CommandFormat cf = new CommandFormat(0, Integer.MAX_VALUE);
         cf.addOptionWithValue(READ_FROM_FILE);
+        cf.addOptionWithValue(PAGE_SIZE);
         cf.parse(args);
         fileName = cf.getOptValue(READ_FROM_FILE);
+        if(cf.getOptValue(PAGE_SIZE) != null) {
+            pageSize = Integer.parseInt(cf.getOptValue(PAGE_SIZE));
+        } else {
+            pageSize = 1;
+        }
     }
 
     @Override
@@ -77,6 +86,13 @@ public class BulkDeleteCommand extends FsCommand {
         args.remove(0);
         this.childArgs = args;
         return pathData;
+    }
+
+    void deleteInBatches(BulkDelete bulkDelete, List<Path> paths) throws IOException {
+        Batch<Path> batches = new Batch<>(paths, pageSize);
+        while(batches.hasNext()) {
+            bulkDelete.bulkDelete(batches.next());
+        }
     }
 
     @Override
@@ -97,6 +113,32 @@ public class BulkDeleteCommand extends FsCommand {
             pathList.addAll(this.childArgs.stream().map(Path::new).collect(Collectors.toList()));
         }
         BulkDelete bulkDelete = basePath.fs.createBulkDelete(basePath.path);
-        bulkDelete.bulkDelete(pathList);
+        deleteInBatches(bulkDelete, pathList);
+    }
+
+    static class Batch<T> {
+        List<T> data;
+        int batchSize;
+        int currentLocation;
+
+        Batch(List<T> data, int batchSize) {
+            this.data = Collections.unmodifiableList(data);
+            this.batchSize = batchSize;
+            this.currentLocation = 0;
+        }
+
+        boolean hasNext() {
+            return this.currentLocation < this.data.size();
+        }
+
+        List<T> next() {
+            List<T> ret = new ArrayList<>();
+            int i = currentLocation;
+            for(; i < Math.min(currentLocation + batchSize, data.size()); i++) {
+                ret.add(this.data.get(i));
+            }
+            currentLocation = i;
+            return ret;
+        }
     }
 }
